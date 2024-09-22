0ca87a597e2a9a35dd7e476585d0728010878184
Samarth100Coder <kumarsamarth272@gmail.com>
2024-09-22 15:49:55 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+print('Square root is: ',math.sqrt(16))
+print('2 to power 4:',math.pow(2,4))
+print('PI:',math.pi)
+print('Floor:',math.floor(2.4354365455))
+print('Ceil:',math.ceil(3.6))
+print('Sin:',math.sin(90))
+print('Cos:',math.cos(90))
+print('Tan:',math.tan(90))
